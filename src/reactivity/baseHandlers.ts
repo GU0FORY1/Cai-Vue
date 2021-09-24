7dd6f6b1e61fe94e4a50771d60b2b2312a103e00
GU0FORY1 <tok4n@hotmail.com>
2021-09-25 00:30:07 +0800

    fix isReactive bug

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -5,7 +5,7 @@ import { ReactiveFlags } from "./reactive";
 function createGetter(isReadonly = false) {
   return function get(target, key) {
     if (key === ReactiveFlags.IS_REACTIVE) {
-      return isReadonly;
+      return !isReadonly;
     } else if (key === ReactiveFlags.IS_READONLY) {
       return isReadonly;
     }
